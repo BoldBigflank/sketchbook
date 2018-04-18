fc1b2aec03a64d4254384dde79d95c1dcc61c7f8
Alex Swan <alex@bold-it.com>
2018-04-18 00:41:26 -0500

    sketchpad added to page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sketchpad from 'sketchpad/lib/sketchpad';
 
 class App extends Component {
+  componentDidMount() {
+    this.sketchpad = new Sketchpad({
+      element: '#sketchpad',
+      width:400,
+      height:400
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <canvas id="sketchpad"></canvas>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
